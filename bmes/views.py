6c0ad166f0def9c1208397073fa4af1261b63d99
Chinedu Ogbu <techsafety2010@yahoo.com>
2019-08-29 09:48:21 +0100

    Commit number 2: This is the second commit

--- bmes/views.py
+++ bmes/views.py
@@ -3,35 +3,11 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import  url_for, redirect 
 from bmes import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+    return redirect(url_for('catalogue.catalogue_view'))
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
